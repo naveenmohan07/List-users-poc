95455be43188536a26023386cc40ba9d8ab5a4a1
naveenkumar_mohanasundaram <nmohanasundaram@presidio.com>
2022-06-27 18:52:09 +0530

    Initial Commit

---
+++ storyboard WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  storyboard WatchKit Extension
+//
+//  Created by NaveenKumar Mohanasundaram on 27/06/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
